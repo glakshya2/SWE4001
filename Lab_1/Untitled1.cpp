cb07092b7a57bd04e15398b69eb9ae70bf180993
glakshya2 <glakshya2@gmail.com>
2024-07-17 18:50:38 +0530

    Complete Lab 1

--- Lab_1/Untitled1.cpp
+++ Lab_1/Untitled1.cpp
@@ -4,87 +4,115 @@
 #include <sstream>
 using namespace std;
 
-
-struct Record{
+struct Record
+{
 	string name;
 	int value;
 };
 
 Record symbolTable[50];
 
+// int stoi(string from) {
+// 	int to = 0;
+// 	stringstream stream;
+// 	stream << from;
+// 	stream >> to;
+// 	return to;
+// }
 
-int stoi(string from) {
-	int to = 0;
-	stringstream stream;
-	stream << from;
-	stream >> to;
-	return to;
-}
-
-int search (string name) {
+int search(string name)
+{
 	int val = 0;
-	cout << "in search" << endl;
-	for (int i = 0; i < 50; i++) {
-		if (symbolTable[i].name.compare(name) == 0) {
-			cout << "name: " << symbolTable[i].name << " value: " << symbolTable[i].value << endl;
+	for (int i = 0; i < 50; i++)
+	{
+		if (symbolTable[i].name.compare(name) == 0)
+		{
 			return symbolTable[i].value;
 		}
 	}
 	return 0;
 }
 
-int main(void) {
+void modify(string name, int value)
+{
+	for (int i = 0; i < 50; i++)
+	{
+		if (symbolTable[i].name.compare(name) == 0)
+		{
+			symbolTable[i].value = value;
+			cout << "Done" << endl;
+		}
+	}
+}
+int main(void)
+{
 	string line;
 	ifstream myFile("./input.txt");
-	
-	if (myFile.is_open()){
+
+	if (myFile.is_open())
+	{
 		int i = 0;
-		while (getline(myFile, line)) {
-			cout << line << '\n';
+		while (getline(myFile, line))
+		{
+
 			size_t position = line.find("=");
-			if (position != string::npos) {
-				cout << "string size = " << line.size() << " string length = " << line.length() << " = at position " << position << endl;
+			if (position != string::npos)
+			{
 				symbolTable[i].name = line.substr(0, position - 1);
-				cout << "name = " << symbolTable[i].name << endl;
 				symbolTable[i].value = stoi(line.substr(position + 1, line.size()));
-				cout << "value = " << symbolTable[i].value << endl;
 				i++;
-			}			
-			cout << "ready" << endl;
+			}
+			cout << "Ready" << endl;
 		}
 		myFile.close();
-	} else {
-		cout <<"unable to open file";
 	}
-	for (int i = 0; i < 50; i++) {
-		cout << symbolTable[i].value << endl;
+	else
+	{
+		cout << "unable to open file";
 	}
-	int option = 0;
-	string options = "Choose option:\n1. Search\n2. Modify\n3. Display all\n4. Exit\n";
-	cout << options;
-	cin >> option;
-	switch (option) {
+	while (true)
+	{
+		int option = 0;
+		string options = "Choose option:\n1. Search\n2. Modify\n3. Display all\n4. Exit\n";
+		cout << options;
+		cin >> option;
+		switch (option)
+		{
 		case 1:
-			{
-			
+		{
 			cout << "Enter name: ";
-			string sName;
-			cin >> sName;
-			cout << "\nValue: " << search << endl;
+			string name;
+			cin >> name;
+			cout << "Value: " << search(name) << endl;
 			break;
 		}
 		case 2:
 		{
-		
 			cout << "Enter name: ";
-			string sName;
-			cin >> sName;
+			string name;
+			cin >> name;
+			cout << "Enter new value: ";
+			int value;
+			cin >> value;
+			modify(name, value);
+			break;
+		}
+		case 3:
+		{
+			cout << "Name\tValue" << endl;
+			for (int i = 0; i < 50; i++)
+			{
+				if (symbolTable[i].name.empty()) {
+					break;
+				}
+				cout << symbolTable[i].name << "\t" << symbolTable[i].value << endl;
+			}
 			break;
 		}
 		default:
 			return 1;
+		}
 	}
-	
-	
+
 	return 0;
 }
